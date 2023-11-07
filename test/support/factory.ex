06abd30d9fe74da69d70701b86340f2264466540
James Rowa <jrowah@gmail.com>
2023-11-07 08:35:23 +0300

    Add factory module for factory definitions

---
+++ test/support/factory.ex
@@ -0,0 +1,10 @@
+defmodule Chatter.Factory do
+  use ExMachina.Ecto, repo: Chatter.Repo
+
+  def chat_room_factory do
+    %Chatter.Chat.Room{
+      # generates auto-incrementing values
+      name: sequence(:name, &"chat room #{&1 + 1}")
+    }
+  end
+end
