fd17f93b41affff24147029caebd7da6e12d0801
James Rowa <jrowah@gmail.com>
2023-11-07 08:45:11 +0300

    List all available chat rooms

--- lib/chatter_web/controllers/chat_room_controller.ex
+++ lib/chatter_web/controllers/chat_room_controller.ex
@@ -1,7 +1,10 @@
 defmodule ChatterWeb.ChatRoomController do
   use ChatterWeb, :controller
 
+  alias Chatter.Chat
+
   def index(conn, _params) do
-    render(conn, :index)
+    chat_rooms = Chat.all_rooms()
+    render(conn, :index, chat_rooms: chat_rooms)
   end
 end
